a7606ac5e2f0528112487f1a1def9e722a70012b
MaksymMedvedchuk <maksim.medvedchuk89@gmail.com>
2023-12-22 20:57:31 +0200

    fixed code

--- src/main/java/com/arestmanagement/security/service/impl/AccessTokenServiceImpl.java
+++ src/main/java/com/arestmanagement/security/service/impl/AccessTokenServiceImpl.java
@@ -1,14 +1,10 @@
 package com.arestmanagement.security.service.impl;
 
-import com.arestmanagement.core.domain.dto.LoginDto;
 import com.arestmanagement.core.domain.entity.Token;
-import com.arestmanagement.core.domain.entity.User;
 import com.arestmanagement.core.domain.enums.TokenType;
 import com.arestmanagement.core.service.DataTimeService;
-import com.arestmanagement.core.service.UserService;
 import com.arestmanagement.security.model.AuthUser;
 import com.arestmanagement.security.service.AccessTokenService;
-import com.arestmanagement.security.service.TokenService;
 import com.arestmanagement.security.tokengenerator.TokenExpirationTime;
 import com.arestmanagement.security.tokengenerator.TokenGeneratorProvider;
 import com.arestmanagement.security.tokengenerator.TokenSecretKey;
@@ -19,31 +15,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class AccessTokenServiceImpl extends TokenGeneratorProvider implements AccessTokenService {
 
-	private final TokenService tokenService;
-
-	private final UserService userService;
-
 	public AccessTokenServiceImpl(
-		final TokenService tokenService,
 		final TokenExpirationTime tokenExpirationTime,
 		final DataTimeService dataTimeService,
-		final TokenSecretKey tokenSecretKey,
-
-		final UserService userService
+		final TokenSecretKey tokenSecretKey
 	) {
 		super(tokenExpirationTime, dataTimeService, tokenSecretKey);
-		this.tokenService = tokenService;
-		this.userService = userService;
 	}
 
 	@Override
 	public Token generateAccessToken(final AuthUser authUser) {
 		final Token generatedToken = generateToken(authUser, TokenType.ACCESS);
-		//tokenService.saveToken(generatedToken);
-		log.debug(
-			"In generateAccessToken: successfully create VerificationToke for user with id: {}",
-			authUser.getId()
-		);
+		log.debug("In generateAccessToken successfully create access token for user with id: {}", authUser.getId());
 		return generatedToken;
 	}
 }
